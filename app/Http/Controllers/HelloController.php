1766da102695dfeef625b2fa028949d322e97b2e
miurakoutarou <koutarousanto@gmail.com>
2022-06-17 14:30:07 +0900

    コミットP84

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,27 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-class HelloController extends Controller
-{
+
 
 //function tag($tag, $txt) {
 //   return "<{$tag}>" . $txt . "</{$tag}>";
 //}
 
-public function index()
+class HelloController extends Controller
 {
-   $data = [
-       'msg'=>'お名前を入力してください。',
-   ];
-   return view('hello.index', $data);
-}
-public function post(Request $request)
-{
-    $msg = $request->msg;
-    $data = [
-        'msg'=>'こんにちは、' . $msg . 'さん！',
-    ];
-    return view('hello.index', $data);
-}
+  
+    public function index()
+    {
+       $data = ['one', 'two', 'three', 'four', 'five'];
+       return view('hello.index', ['data'=>$data]);
+    }
+    
+    
 
 }
