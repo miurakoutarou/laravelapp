9d12b9d12d634819ee3f60b70594b6fb639549c2
miurakoutarou <koutarousanto@gmail.com>
2022-07-04 12:17:42 +0900

    コミット213

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,12 +11,12 @@ use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
-   public function index(Request $request)
-   {
-       $items = DB::select('select * from people');
+    public function index(Request $request)
+    {
+       $items = DB::table('people')->get();
        return view('hello.index', ['items' => $items]);
-   }
-
+    }
+    
    public function post(Request $request)
    {
        $items = DB::select('select * from people');
@@ -38,5 +38,40 @@ class HelloController extends Controller
        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
        return redirect('/hello');
    }
+   public function edit(Request $request)
+{
+   $param = ['id' => $request->id];
+   $item = DB::select('select * from people where id = :id', $param);
+   return view('hello.edit', ['form' => $item[0]]);
+}
+
+public function update(Request $request)
+{
+   $param = [
+       'id' => $request->id,
+       'name' => $request->name,
+       'mail' => $request->mail,
+       'age' => $request->age,
+   ];
+   DB::update('update people set name =:name, mail = :mail, age = :age where id = :id', $param);
+   return redirect('/hello');
+}
+public function show(Request $request)
+{
+   $min = $request->min;
+   $max = $request->max;
+   $items = DB::table('people')
+       ->whereRaw('age >= ? and age <= ?',
+        [$min, $max])->get();
+   return view('hello.show', ['items' => $items]);
+}
+
+
+
+
+
+
+
+
 
 }
