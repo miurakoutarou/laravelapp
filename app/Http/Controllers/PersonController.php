59bc2ff1ba913b281aee2ef4f89fca32a41ec39f
miurakoutarou <koutarousanto@gmail.com>
2022-07-06 11:26:57 +0900

    コミット245

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -12,8 +12,21 @@ class PersonController extends Controller
        $items = Person::all();
        return view('person.index', ['items' => $items]);
    }
+
    public function getData()
 {
    return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
 }
+public function find(Request $request)
+{
+   return view('person.find',['input' => '']);
+}
+
+public function search(Request $request)
+{
+   $item = Person::find($request->input);
+   $param = ['input' => $request->input, 'item' => $item];
+   return view('person.find', $param);
+}
+
 }
