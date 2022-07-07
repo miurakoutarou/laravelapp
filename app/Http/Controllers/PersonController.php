4080f7aeabb6bb9b8a60cd4f22e6d8239da31427
miurakoutarou <koutarousanto@gmail.com>
2022-07-07 12:19:56 +0900

    コミット265

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Person;
 use Illuminate\Http\Request;
-
+use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson; 
 class PersonController extends Controller
 {
    public function index(Request $request)
@@ -24,9 +25,70 @@ public function find(Request $request)
 
 public function search(Request $request)
 {
-   $item = Person::where('name', $request->input)->first();
+   $min = $request->input * 1;
+   $max = $min + 10;
+   $item = Person::ageGreaterThan($min)->ageLessThan($max)->first();
    $param = ['input' => $request->input, 'item' => $item];
    return view('person.find', $param);
+}
+
+public function add(Request $request)
+{
+   return view('person.add');
+}
+
+public function create(Request $request)
+{
+   $this->validate($request, Person::$rules);
+   $person = new Person;
+   $form = $request->all();
+   unset($form['_token']);
+   $person->fill($form)->save();
+   return redirect('/person');
+}
+
+public function edit(Request $request)
+{
+   $person = Person::find($request->id);
+   return view('person.edit', ['form' => $person]);
+}
+
+public function update(Request $request)
+{
+   $this->validate($request, Person::$rules);
+   $person = Person::find($request->id);
+   $form = $request->all();
+   unset($form['_token']);
+   $person->fill($form)->save();
+   return redirect('/person');
+}
+
+
+public function scopeNameEqual($query, $str)
+{
+   return $query->where('name', $str);
+}
+
+public function scopeAgeGreaterThan($query, $n)
+{
+   return $query->where('age','>=', $n);
+}
+
+public function scopeAgeLessThan($query, $n)
+{
+   return $query->where('age', '<=', $n);
+}
+
+public function delete(Request $request)
+{
+   $person = Person::find($request->id);
+   return view('person.del', ['form' => $person]);
+}
+
+public function remove(Request $request)
+{
+   Person::find($request->id)->delete();
+   return redirect('/person');
 }
 
 
